7a3442ae57c1752bc513540e4ffddc377022dcc9
zhnnnnn <475816908@qq.com>
2016-05-03 17:30:50 +0800

    Initial Commit

---
+++ ZHNwebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHNwebImage
+//
+//  Created by zhn on 16/5/3.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHNwebImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZHNwebImage
+//
+//  Created by zhn on 16/5/3.
+//  Copyright © 2016年 zhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
